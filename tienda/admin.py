986f13c23d2f90fc5edebbe17d88128feec00916
Matiasbcg <145684006+Matiasbcg@users.noreply.github.com>
2024-04-25 12:52:36 -0400

    Delete tienda directory

--- tienda/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Usuario , Marca_Consola , Consola
-# Register your models here.
-admin.site.register(Usuario)
-admin.site.register(Marca_Consola)
-admin.site.register(Consola)

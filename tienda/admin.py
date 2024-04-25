5c7d47e453f544bc6cdda9315950575d05b2a340
Matiasbcg <145684006+Matiasbcg@users.noreply.github.com>
2024-04-25 12:58:20 -0400

    Servicios Api
    Se añade 3 servicios APIS

---
+++ tienda/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Usuario , Marca_Consola , Consola
+# Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Marca_Consola)
+admin.site.register(Consola)

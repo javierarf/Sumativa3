5e97c93ab294bbf98b7365d3013c3f462a85cde8
Matiasbcg <145684006+Matiasbcg@users.noreply.github.com>
2024-04-27 12:31:28 -0400

    Delete rest_api directory

--- rest_api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views 
-
-urlpatterns = [
-    path('usuarios/' , views.lista_usuarios , name='lista_usuarios'),
-    path('consolas/', views.lista_consolas, name='lista_consolas'),
-    path('consolas/<id>' , views.vista_consola , name='vista_consola')
-]
